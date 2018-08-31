0279b3d4cb0ffd959acf8d045b6a72e669055b5f
Noedith <noedith7@gmail.com>
2018-08-30 22:11:45 -0500

    Redireccion a /boletines

--- boletines/javascript/main.js
+++ boletines/javascript/main.js
@@ -14,7 +14,7 @@ links.forEach(function (link) {
     content.classList.add("animated");
 
     setTimeout(function() {
-      location.href = "/";
+      location.href = "/boletines";
     },600);
     return false;
   });
